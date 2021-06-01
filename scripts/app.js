a78aca9fc12e0a234e59401935f231ad77b777e8
Jose Rios <joseriosdev@gmail.com>
2021-06-01 14:30:00 -0500

    board can be added and searched, ui is also working nice

--- scripts/app.js
+++ scripts/app.js
@@ -1,65 +1,101 @@
 // visit api link documentation here
 // https://developer.atlassian.com/cloud/trello/guides/rest-api/api-introduction/
 
-const BTN_ADD = document.getElementById("btn-add-item");
-const FORM = document.getElementById("form");
-const MODAL_FORM = document.getElementById("modal-form");
-const MODAL_BTN = document.getElementById("search-btn");
+const addBtn = document.getElementById('btn-add-item');
+const form = document.getElementById('form');
+const modalForm = document.getElementById('modal-form');
+const modalBtn = document.getElementById('search-btn');
+const checkPattern = [false, true, true];
 
 // only 2 events, one for each btn (add & search)
-FORM.addEventListener("submit", addItem);
-MODAL_BTN.addEventListener("click", searchItem);
+addPatternCheckerToInputs();
+form.addEventListener('submit', addItem);
+modalBtn.addEventListener('click', searchItem);
 
 function addItem(evt) {
 	evt.preventDefault();
 
-	const BOARD = document.getElementById("board").value;
-	const LIST = document.getElementById("list").value;
-	const CARD = document.getElementById("card").value;
+	const board = document.getElementById('board');
+	const listValue = document.getElementById('list');
+	const cardValue = document.getElementById('card');
+	const ui = new UI('alert-message');
+	const trello = new ApiCalls();
 
-	let apiCalls = new ApiCalls();
-	let ui = new UI();
 
-	apiCalls.postBoard(BOARD)
-		.then((res) => {
-			console.log(res);
-			ui.alertMessage("Item added", "bg-success");
-		});
-
-	apiCalls.getBoards()
-		.then((res) => console.log(res));
-	
+	trello.getBoards().then(res => {
+		if (res.length < 10) {
+			if (!checkPattern.includes(false)) {
+				trello.postBoard(board.value)
+				.then((res) => {
+					console.log(res);
+					ui.alertMessage('Board added :D', 'bg-success');
+				});
+			} else {
+				ui.alertMessage('Check what you wrote o.O', 'bg-info');
+			}
+		} else {
+			ui.alertMessage('You\'ve reached the max amount of boards, delete some of them. :/', 'bg-danger');
+		}	
+	});
 }
 
 function searchItem(evt) {
 	evt.preventDefault();
-	console.log("entre en el evento");
-	const SEARCH_BAR = document.getElementById("search-bar").value;
+	const trello = new ApiCalls();
+	const searchBar = document.getElementById('search-bar').value;
 
-	let apiCalls = new ApiCalls();
-
-	apiCalls.getBoards()
-		.then((res) => locateBoardNames(res, SEARCH_BAR))
+	trello.getBoards()
+		.then((res) => locateBoardNames(res, searchBar))
 		.catch((err) => console.log(err));
 }
 
 
 function locateBoardNames(boardListObj, data) {
-	for (let i=0; i<boardListObj.length; i++) {
-		console.log("entre en el for");
-		if (data === boardListObj[i].name) {
-			let searchDisplay = document.getElementById("search-display");
-			searchDisplay.innerHTML = ""; //for cleaning when creating a new search
-			
-			let ul = document.createElement("ul");
+	console.log('boards length: ' + boardListObj.length);
+	console.log('response' + boardListObj);
+	console.log(boardListObj);
 
-			ul.innerHTML = `<li>${boardListObj[i].name}</li>`;
+	for (let i = 0; i < boardListObj.length; i++) {
+		console.log(boardListObj[i].name);
+		console.log(data);
+		if (data.toLowerCase() === boardListObj[i].name.toLowerCase()) {
+			let searchDisplay = document.getElementById('search-display');
+			searchDisplay.innerHTML = ''; //for cleaning when creating a new search
+			
+			let ul = document.createElement('ul');
+			ul.classList.add('text-dark');
+
+			ul.innerHTML = `<li>Card Name: ${boardListObj[i].name}</li>`;
+			console.log(ul)
 
 			searchDisplay.appendChild(ul);
+			break;
 		} 				
-		else if (i == boardListObj.length-1) {
-			let ui = new UI();
-			ui.alertModalMessage("Nothing to show", "bg-warning");
+		else if (i === boardListObj.length-1) {
+			let ui = new UI('search-display');
+			ui.alertMessage('Nothing to show', 'bg-danger');
 		}
+	}
+}
+
+function addPatternCheckerToInputs() {
+	let inputs = document.querySelectorAll('input[name="board-item"]');
+	
+	for (let i = 0; i < inputs.length; i++) {
+		const regex = /^(?! )([A-Za-z0-9 ]){1,20}$/;
+		const index = i;
+
+		inputs[i].addEventListener('blur', (evt) => {
+
+      if (evt.target.value !== '') {
+        if (regex.test(evt.target.value)) {
+          inputs[i].classList.remove('is-invalid');
+          checkPattern[index] = true;
+        } else {
+          inputs[i].classList.add('is-invalid');
+          checkPattern[index] = false;
+        }
+      }
+    })
 	}
 }
--- scripts/ui.js
+++ scripts/ui.js
@@ -1,7 +1,10 @@
 class UI {
-  constructor() {
-    this.messageDisplayer = document.getElementById("alert-message");
-    this.modalMessageDisplayer = document.getElementById("search-display");
+  constructor(elementId = undefined) {
+    this.messageDisplayer = 
+      typeof elementId === 'string' ? 
+        document.getElementById(elementId) 
+        : 
+        elementId;
   }
 
   alertMessage(message, bgClassNameColor) {
@@ -10,19 +13,7 @@ class UI {
 
     setTimeout(() => {
       this.messageDisplayer.classList.remove(bgClassNameColor);
-      this.messageDisplayer.innerHTML = "";
-    }, 3000)
-
-  }
-  
-  alertModalMessage(message, bgClassNameColor) {
-    this.modalMessageDisplayer.classList.add(bgClassNameColor);
-    this.modalMessageDisplayer.innerHTML = message;
-
-    setTimeout(() => {
-      this.modalMessageDisplayer.classList.remove(bgClassNameColor);
-      this.modalMessageDisplayer.innerHTML = "";
-    }, 3000)
-
+      this.messageDisplayer.innerHTML = '';
+    }, 4000)
   }
 }
